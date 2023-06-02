ba1cf91ac22e9f01a9c3175f897d926be57abc80
gog1na <goginashvili.giorgi.gg@gmail.com>
2023-06-02 16:10:26 +0400

    Log In & Register Screens

---
+++ RealTimeChatApp/Controllers/Login&Registration/RegisterViewController.swift
@@ -0,0 +1,218 @@
+//
+//  RegisterViewController.swift
+//  RealTimeChatApp
+//
+//  Created by Giorgi Goginashvili on 6/2/23.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    
+    // Properties
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.clipsToBounds = true
+        return scrollView
+    }()
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "person.circle")
+        imageView.tintColor = .gray
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private let firstNameField: UITextField = {
+        let field = UITextField()
+        field.autocorrectionType = .no
+        field.autocapitalizationType = .none
+        field.returnKeyType = .continue
+        field.layer.cornerRadius = 12
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.lightGray.cgColor
+        field.placeholder = "First Name..."
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 0))
+        field.leftViewMode = .always
+        field.backgroundColor = .white
+        return field
+    }()
+    
+    private let lastNameField: UITextField = {
+        let field = UITextField()
+        field.autocorrectionType = .no
+        field.autocapitalizationType = .none
+        field.returnKeyType = .continue
+        field.layer.cornerRadius = 12
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.lightGray.cgColor
+        field.placeholder = "Last Name..."
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 0))
+        field.leftViewMode = .always
+        field.backgroundColor = .white
+        return field
+    }()
+    
+    private let emailField: UITextField = {
+        let field = UITextField()
+        field.autocorrectionType = .no
+        field.autocapitalizationType = .none
+        field.returnKeyType = .continue
+        field.layer.cornerRadius = 12
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.lightGray.cgColor
+        field.placeholder = "Email Address..."
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 0))
+        field.leftViewMode = .always
+        field.backgroundColor = .white
+        return field
+    }()
+    
+    private let passwordField: UITextField = {
+        let field = UITextField()
+        field.autocorrectionType = .no
+        field.autocapitalizationType = .none
+        field.returnKeyType = .done
+        field.layer.cornerRadius = 12
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.lightGray.cgColor
+        field.placeholder = "Password..."
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 0))
+        field.leftViewMode = .always
+        field.backgroundColor = .white
+        field.isSecureTextEntry = true
+        return field
+    }()
+    
+    private let registerButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Register", for: .normal)
+        button.backgroundColor = .systemGreen
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 12
+        button.layer.masksToBounds = true
+        button.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
+        return button
+    }()
+
+    // Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "Register"
+        view.backgroundColor = .white
+        
+        firstNameField.delegate = self
+        lastNameField.delegate = self
+        emailField.delegate = self
+        passwordField.delegate = self
+        
+        registerButton.addTarget(self, action: #selector(registerButtonTapped), for: .touchUpInside)
+        
+        // Add subviews
+        view.addSubview(scrollView)
+        scrollView.addSubview(imageView)
+        scrollView.addSubview(firstNameField)
+        scrollView.addSubview(lastNameField)
+        scrollView.addSubview(emailField)
+        scrollView.addSubview(passwordField)
+        scrollView.addSubview(registerButton)
+        
+        scrollView.isUserInteractionEnabled = true
+        imageView.isUserInteractionEnabled = true
+        
+        let gesture = UITapGestureRecognizer(target: self,
+                                             action: #selector(didTapChangeProfilePic))
+        gesture.numberOfTouchesRequired = 1
+        gesture.numberOfTapsRequired = 1
+        imageView.addGestureRecognizer(gesture)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        scrollView.frame = view.bounds
+        let size = scrollView.width / 3
+        
+        imageView.frame = CGRect(x: (scrollView.width - size) / 2,
+                                 y: 20,
+                                 width: size,
+                                 height: size)
+        firstNameField.frame = CGRect(x: 30,
+                                      y: imageView.bottom + 10,
+                                      width: scrollView.width - 60,
+                                      height: 52)
+        lastNameField.frame = CGRect(x: 30,
+                                      y: firstNameField.bottom + 10,
+                                      width: scrollView.width - 60,
+                                      height: 52)
+        emailField.frame = CGRect(x: 30,
+                                      y: lastNameField.bottom + 10,
+                                      width: scrollView.width - 60,
+                                      height: 52)
+        passwordField.frame = CGRect(x: 30,
+                                      y: emailField.bottom + 10,
+                                      width: scrollView.width - 60,
+                                      height: 52)
+        registerButton.frame = CGRect(x: 30,
+                                      y: passwordField.bottom + 10,
+                                      width: scrollView.width - 60,
+                                      height: 52)
+    }
+    
+    // Methods
+    @objc private func didTapChangeProfilePic() {
+        print("Change picture called")
+    }
+    
+    @objc private func registerButtonTapped() {
+        firstNameField.resignFirstResponder()
+        lastNameField.resignFirstResponder()
+        emailField.resignFirstResponder()
+        passwordField.resignFirstResponder()
+        
+        guard let firstName = firstNameField.text,
+              let lastName = lastNameField.text,
+              let email = emailField.text,
+              let password = passwordField.text,
+              !firstName.isEmpty,
+              !lastName.isEmpty,
+              !email.isEmpty,
+              !password.isEmpty,
+              password.count >= 6 else {
+            alertUserLoginError()
+            return
+        }
+        
+        // Firebase register
+    }
+    
+    func alertUserLoginError() {
+        let alert = UIAlertController(title: "Woops",
+                                      message: "Please enter all information to create a new account.",
+                                      preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Dismiss",
+                                      style: .cancel, handler: nil))
+        present(alert, animated: true)
+    }
+
+}
+
+
+extension RegisterViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == firstNameField {
+            lastNameField.becomeFirstResponder()
+        } else if textField == lastNameField {
+            emailField.becomeFirstResponder()
+        } else if textField == emailField {
+            passwordField.becomeFirstResponder()
+        } else if textField == passwordField {
+            //
+        }
+        return true
+    }
+    
+}
---
+++ RealTimeChatApp/Resources/Extensions.swift
@@ -0,0 +1,37 @@
+//
+//  Extensions.swift
+//  RealTimeChatApp
+//
+//  Created by Giorgi Goginashvili on 6/2/23.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+    public var width: CGFloat {
+        return self.frame.size.width
+    }
+    
+    public var height: CGFloat {
+        return self.frame.size.height
+    }
+    
+    public var top: CGFloat {
+        return self.frame.origin.y
+    }
+    
+    public var bottom: CGFloat {
+        return self.frame.size.height + self.frame.origin.y
+    }
+    
+    public var left: CGFloat {
+        return self.frame.origin.x
+    }
+    
+    public var right: CGFloat {
+        return self.frame.size.width + self.frame.origin.x
+    }
+    
+}
