beba4b38b236a1880c9e4ec10323e40359df28f0
gog1na <goginashvili.giorgi.gg@gmail.com>
2023-06-14 11:21:01 +0400

    Upload Photos to Firebase Storage

--- RealTimeChatApp/Controllers/Login&Registration/RegisterViewController.swift
+++ RealTimeChatApp/Controllers/Login&Registration/RegisterViewController.swift
@@ -220,9 +220,30 @@ class RegisterViewController: UIViewController {
                     return
                 }
                 
-                DatabaseManager.shared.insertUser(with: ChatAppUser(firstName: firstName,
-                                                                    lastName: lastName,
-                                                                    emailAddress: email))
+                let chatUser = ChatAppUser(firstName: firstName,
+                                           lastName: lastName,
+                                           emailAddress: email)
+                DatabaseManager.shared.insertUser(with: chatUser, completion: { success in
+                    if success {
+                        // Upload image
+                        guard let image = self.imageView.image, let data = image.pngData() else {
+                            
+                            return
+                        }
+                        let fileName = chatUser.profilePictureFileName
+                        StorageManager.shared.uploadProfilePicture(with: data,
+                                                                   fileName: fileName,
+                                                                   completion: { result in
+                            switch result {
+                            case .success(let downloadUrl):
+                                UserDefaults.standard.setValue(downloadUrl, forKey: "profile_picture_url")
+                                print(downloadUrl)
+                            case .failure(let error):
+                                print("Storage manager error: \(error)")
+                            }
+                        })
+                    }
+                })
                 
                 self.navigationController?.dismiss(animated: true, completion: nil)
             })
