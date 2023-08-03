62ebb5c59fd8e7d5b2b2e47c0fe7425fa6b6f0e5
gog1na <goginashvili.giorgi.gg@gmail.com>
2023-08-03 15:23:23 +0400

    Handling Messages
    Update latest message

--- RealTimeChatApp/Controllers/Login&Registration/LoginViewController.swift
+++ RealTimeChatApp/Controllers/Login&Registration/LoginViewController.swift
@@ -170,6 +170,7 @@ class LoginViewController: UIViewController {
                   let lastName = user.profile?.familyName else { return }
             
             UserDefaults.standard.set(email, forKey: "email")
+            UserDefaults.standard.set("\(firstName) \(lastName)", forKey: "name")
             
             Auth.auth().signIn(with: credential) { authResult, error in
                 guard authResult != nil, error == nil else {
@@ -249,6 +250,21 @@ class LoginViewController: UIViewController {
             
             let user = result.user
             
+            let safeEmail = DatabaseManager.safeEmail(emailAddress: email)
+            DatabaseManager.shared.getDataFor(path: safeEmail, completion: { result in
+                switch result {
+                case .success(let data):
+                    guard let userData = data as? [String: Any],
+                          let firstName = userData["first_name"],
+                          let lastName = userData["last_name"] else {
+                        return
+                    }
+                    UserDefaults.standard.set("\(firstName) \(lastName)", forKey: "name")
+                case .failure(let error):
+                    print("Failed to read data with error \(error)")
+                }
+            })
+            
             UserDefaults.standard.set(email, forKey: "email")
             
             print("Logged in User: \(user)")
@@ -326,6 +342,7 @@ extension LoginViewController: LoginButtonDelegate {
             }
             
             UserDefaults.standard.set(email, forKey: "email")
+            UserDefaults.standard.set("\(firstName) \(lastName)", forKey: "name")
             
             DatabaseManager.shared.userExists(with: email, completion: { exists in
                 if !exists {
